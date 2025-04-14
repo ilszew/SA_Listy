65a09461bf4c7d6707ac4814fc3f6979f1df2369
Kornel Lewandowski <280101@student.pwr.edu.pl>
2025-04-14 11:29:10 +0200

    doubly linked list was implemented with some tests in main

--- doublyLinkedList.h
+++ doublyLinkedList.h
@@ -3,7 +3,116 @@
 
 template <typename T>
 class DoublyLinkedList {
+private:
+    struct Node {
+        T data;
+        Node* prev;
+        Node* next;
 
+        Node(const T& value) : data(value), prev(nullptr), next(nullptr) {}
+    };
+
+    Node* head;
+    Node* tail;
+    int size;
+
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+
+    ~DoublyLinkedList() {
+        clear();
+    }
+
+    void pushFront(const T& value) {
+        Node* newNode = new Node(value);
+
+        if (head == nullptr) {
+            head = tail = newNode;
+        } else {
+            newNode->next = head;
+            head->prev = newNode;
+            head = newNode;
+        }
+
+        size++;
+    }
+
+    void pushBack(const T& value) {
+        Node* newNode = new Node(value);
+
+        if (tail == nullptr) {
+            head = tail = newNode;
+        } else {
+            newNode->prev = tail;
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    void popFront() {
+        if (head == nullptr) return;
+
+        Node* temp = head;
+
+        if (head == tail) {
+            head = tail = nullptr;
+        } else {
+            head = head->next;
+            head->prev = nullptr;
+        }
+
+        delete temp;
+        size--;
+    }
+
+    // Usuń element z końca listy
+    void popBack() {
+        if (tail == nullptr) return;
+
+        Node* temp = tail;
+
+        if (head == tail) {
+            head = tail = nullptr;
+        } else {
+            tail = tail->prev;
+            tail->next = nullptr;
+        }
+
+        delete temp;
+        size--;
+    }
+
+    void clear() {
+        while (head != nullptr) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+
+        head = tail = nullptr;
+        size = 0;
+    }
+
+    bool isEmpty() const {
+        return size == 0;
+    }
+
+    int getSize() const {
+        return size;
+    }
+
+    void print() const {
+        Node* current = head;
+
+        std::cout << "List: ";
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
 
 };
 
--- main.cpp
+++ main.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
 #include "singlyLinkedList.h"
 #include "arrayList.h"
+#include "doublyLinkedList.h"
 
 int main() {
 
-    SinglyLinkedList<int> singlyList;
-
-    singlyList.addLast(10);
-    singlyList.addLast(20);
-    singlyList.addFirst(5);
-    singlyList.addAt(2, 15);
-    std::cout << "SinglyLinkedList: rozmier = " << singlyList.getSize() << std::endl;
-
-    // Usuwanie elementów
-    singlyList.removeFirst();
-    singlyList.removeLast();
-    std::cout << "Po usunieciu: rozmiar = " << singlyList.getSize() << std::endl;
-
-    std::cout << "Czy 10 jest na liscie? " << std::endl;
-    if (singlyList.search(10)) {
-        std::cout <<"Tak";
-    }else {
-        std::cout <<"Nie";
-    }
     ArrayList<int> arrayList;
+    SinglyLinkedList<int> singlyList;
+    DoublyLinkedList<int> lista;
+
+
     arrayList.DodajK(10);
     arrayList.DodajK(20);
     arrayList.DodajP(5);
     arrayList.DodajLos(2, 15);
     std::cout << "ArrayList: rozmiar = " << arrayList.wielkosc() << std::endl;
     arrayList.wyswietl();
-    
+
     arrayList.UsunP();
     arrayList.UsunK();
     arrayList.UsunLos(1);
@@ -44,5 +30,49 @@ int main() {
     else {
         std::cout << "Nie";
     }
+
+    singlyList.addLast(10);
+    singlyList.addLast(20);
+    singlyList.addFirst(5);
+    singlyList.addAt(2, 15);
+    std::cout << "SinglyLinkedList: rozmiar = " << singlyList.getSize() << std::endl;
+
+
+    singlyList.removeFirst();
+    singlyList.removeLast();
+    std::cout << "Po usunieciu: rozmiar = " << singlyList.getSize() << std::endl;
+
+    std::cout << "Czy 10 jest na liscie? " << std::endl;
+    if (singlyList.search(10)) {
+        std::cout <<"Tak";
+    }else {
+        std::cout <<"Nie";
+    }
+
+    std::cout << "\nElementy na koniec listy:" << std::endl;
+    lista.pushBack(10);
+    lista.pushBack(20);
+    lista.pushBack(30);
+
+    lista.print();
+    std::cout << "Rozmiar listy: " << lista.getSize() << std::endl;
+
+    std::cout << "Elementy na początek listy:" << std::endl;
+    lista.pushFront(5);
+    lista.pushFront(1);
+
+    lista.print();
+    std::cout << "Rozmiar listy: " << lista.getSize() << std::endl;
+
+    std::cout << "\nUsuwamy element z początku listy:" << std::endl;
+    lista.popFront();
+    lista.print();
+
+    std::cout << "\nUsuwamy element z końca listy:" << std::endl;
+    lista.popBack();
+    lista.print();
+
+    std::cout << "\nCzyszczenie listy" << std::endl;
+    lista.clear();
     return 0;
 }
